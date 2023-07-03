7002f9fa8e54e78b7730fb3dc80b0d154b1a3a89
marcilia.guilger <marcilia.guilger@ifood.com.br>
2023-07-03 17:53:51 -0300

    separate packages

--- src/main/kotlin/br/com/dog/demoapi/controller/MathController.kt
+++ src/main/kotlin/br/com/dog/demoapi/controller/MathController.kt
@@ -1,6 +1,8 @@
-package br.com.dog.demoapi
+package br.com.dog.demoapi.controller
 
+import br.com.dog.demoapi.converters.NumberConverter
 import br.com.dog.demoapi.exceptions.UnsupportedMathOperationException
+import br.com.dog.demoapi.math.SimpleMath
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -9,26 +11,15 @@ import java.util.concurrent.atomic.AtomicLong
 @RestController
 class MathController {
     val counter: AtomicLong = AtomicLong()
+    private val math: SimpleMath = SimpleMath()
 
     @RequestMapping(value = ["/sum/{numberOne}/{numberTwo}"])
 
     fun sum(@PathVariable(value="numberOne") numberOne: String?,
             @PathVariable(value="numberTwo") numberTwo: String?
         ):Double {
-        if(!isNumeric(numberOne) || !isNumeric(numberTwo))
+        if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo))
             throw UnsupportedMathOperationException("Please set a numeric value")
-        return convertToDouble(numberOne) + convertToDouble(numberTwo)
-    }
-
-    private fun convertToDouble(strNumber: String?): Double {
-        if(strNumber.isNullOrBlank()) return 0.0
-        val number = strNumber.replace(",".toRegex(),".")
-        return if(isNumeric(number)) number.toDouble() else 0.0
-    }
-
-    private fun isNumeric(strNumber: String?): Boolean {
-        if (strNumber.isNullOrBlank()) return false
-        val number = strNumber.replace(",".toRegex(),".")
-        return number.matches("""[-+]?[0-9]*\.?[0-9]+""".toRegex())
+        return math.sum(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo))
     }
 }
